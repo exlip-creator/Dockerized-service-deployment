7e7b40674bd0d4b3ff3e7011b1617228470fab82
exlip-creator <demyan_permyakov@bk.ru>
2026-07-21 12:37:43 +0900

    Added /secret page and auth for it

--- service.js
+++ service.js
@@ -6,8 +6,33 @@ app.get("/", (req, res) => {
     res.send("Hello, dear guest!");
 });
 
-const PORT = process.env.PORT;
+app.get("/secret", (req, res) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.setHeader("WWW-Authenticate", 'Basic realm="Secret area"');
+        return res.status(401).send("Unauthorized: No token provided");
+    }
+
+    const auth = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
+    const username = auth[0];
+    const password = auth[1];
+
+    console.log("Введено пользователем:", { username, password }); 
+    console.log("Ожидается из .env:", { 
+        envUser: process.env.ADMIN_NAME, 
+        envPass: process.env.PASSWORD 
+    });
+
+    if (username === process.env.ADMIN_NAME && password === process.env.PASSWORD){
+        res.send(process.env.SECRET_MESSAGE);
+    } else {
+        res.setHeader('WWW-Authenticate', 'Basic realm="Secure Area"');
+        res.status(401).send("Unauthorized: Invalid credentials");
+    }
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Сервис запущен на порту ${PORT}");
+    console.log(`Сервис запущен на порту ${PORT}`);
 });
